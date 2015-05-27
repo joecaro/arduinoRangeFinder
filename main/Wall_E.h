7fbae14e3582122044ead25c1822a82bd0bce3c0
bencarothers <benjamin.carothers13@ncf.edu>
2015-05-27 10:17:17 -0400

    cleaning up the .h file

--- main/Wall_E.h
+++ main/Wall_E.h
@@ -1,21 +1,19 @@
 #ifndef _Wall_E_H_
 #define _Wall_E_H_
 #include "Arduino.h"
-//add your includes for the project Wall_E here
 #include "NewPing.h"
 #include "rgb_lcd.h"
 #include "inttypes.h"
 
-//end of add your includes here
 #ifdef __cplusplus
 extern "C" {
 #endif
 void loop();
 void setup();
 #ifdef __cplusplus
-} // extern "C"
+}
 #endif
-//add your function definitions for the project Wall_E here
+
 /*
  * takes pingTime, calculates the distance in cm by dividing pingTime by 57
  * (57uS for sound to travel 2cm)
@@ -31,7 +29,9 @@ float calculatePingDistance(float pingTime){
  * takes distance1 and distance2 and subtracts them to find the change in cm
  * divides the change in distance by the change in time to find velocity
  * returns velocity
+ *
  */
+
 float calculateAvgVelocity(float distance1, float distance2, float time){
   float deltaDistance = distance2-distance1;
   float velocity = deltaDistance/time;
@@ -42,8 +42,4 @@ void printVelocity(float velocity, rgb_lcd lcd){
         lcd.print(velocity);
   lcd.write("cm/s");
 }
-
-
-
-//Do not add code below this line
 #endif /* _Wall_E_H_ */
